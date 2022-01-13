e99cc84eac1e7a414d72cf9dc5df5a9e50a133e2
thervieu <thibault99hervieu@gmail.com>
2022-01-13 18:53:32 +0100

    describe with my funct

--- describe.py
+++ describe.py
@@ -1,6 +1,60 @@
 import sys
 import numpy as np
 import pandas as pd
+import math
+
+
+def mean(x):
+    return sum(i for i in x) / len(x)
+
+def median(x):
+    n = len(x)
+    s = sorted(x)
+    return (s[n//2-1]/2.0+s[n//2]/2.0, s[n//2])[n % 2] if n else None
+samples = sorted([28, 12, 8, 27, 16, 31, 14, 13, 19, 1, 1, 22, 13])
+
+def median(x):
+    x.sort()
+    n = len(x)
+    if n % 2 == 0:
+        median = (x[(n//2)]+x[(n//2-1)])/2
+    else:
+        median = x[(n//2)]
+    return median
+
+def first_quartile(x):
+    x.sort()
+    return median(x[:len(x)//2])
+
+def third_quartile(x):
+    x.sort()
+    return median(x[len(x)//2:])
+
+
+def percentile(x, p):
+    n = len(x)
+    s = sorted(x)
+    return (s[floor(p/100*n)-1]/2.0+s[floor(p/100*n)]/2.0, s[floor(p/100*n)])[n % 2] if n else None
+
+def std(x):
+    m = mean(x)
+    n = len(x)
+    return math.sqrt(sum((nb - m)**2 for nb in x) / n)
+
+def min_(x):
+    min_ = x[0]
+    for elem in x:
+        if elem < min_:
+            min_ = elem
+    return min_
+
+def max_(x):
+    max_ = x[0]
+    for elem in x:
+        if elem > max_:
+            max_ = elem
+    return max_
+
 
 if len(sys.argv) != 2:
     print('usage: python <data_set.csv>')
@@ -19,18 +73,19 @@ for str_ in ['Arithmancy','Astronomy','Herbology','Defense Against the Dark Arts
 for str_ in ['Arithmancy','Astronomy','Herbology','Defense Against the Dark Arts','Divination','Muggle Studies','Ancient Runes','History of Magic','Transfiguration','Potions','Care of Magical Creatures','Charms','Flying']:
     featureXX = []
     getMyData = df[str_]
-    featureXX.append(getMyData.count())
-    featureXX.append(np.mean(getMyData))
-    featureXX.append(np.std(getMyData))
-    featureXX.append(np.min(getMyData))
-    featureXX.append(np.quantile(getMyData, 0.25))
-    featureXX.append(np.quantile(getMyData, 0.5))
-    featureXX.append(np.quantile(getMyData, 0.75))
-    featureXX.append(np.max(getMyData))
+    getMyData = list(getMyData)
+    featureXX.append(len(getMyData))
+    featureXX.append(mean(getMyData))
+    featureXX.append(std(getMyData))
+    featureXX.append(min_(getMyData))
+    featureXX.append(first_quartile(getMyData))
+    featureXX.append(median(getMyData))
+    featureXX.append(third_quartile(getMyData))
+    featureXX.append(max_(getMyData))
     allFeatures.append(featureXX)
 
 # print accordingly
-print('\n{}{}{}{}{}{}{}{}{}{}{}{}{}{}'.format(''.rjust(15),'Arithm.'.rjust(15),
+print('\n{}{}{}{}{}{}{}{}{}{}{}{}{}{}'.format(''.rjust(15),'Arithmancy'.rjust(15),
     'Astronomy'.rjust(15),'Herbology'.rjust(15),'Def. Ag. DA'.rjust(15),
     'Divination'.rjust(15),'Muggle Stud.'.rjust(15),'Anc. Runes'.rjust(15),
     'Hist. of Mag.'.rjust(15),'Transfigur.'.rjust(15),'Potions'.rjust(15),
